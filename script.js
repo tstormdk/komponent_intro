5c720ba00bf8b21dcf6684ba0aaa87d2252e1291
tstormdk <tstormdk@gmail.com>
2020-04-15 00:56:34 +0200

    Komponent ready
    Mangler at flytte css ud i nye stylesheets.

---
+++ script.js
@@ -0,0 +1,30 @@
+const whopper = document.getElementById("ikonx");
+
+// const span = document.querySelectorAll(".burger-box span");
+
+whopper.addEventListener("click", menuRollOut);
+window.addEventListener('resize', RemoveClass);
+
+function menuRollOut() {
+    const navBox = document.querySelector(".menu");
+    navBox.classList.toggle("visible");
+}
+
+function RemoveClass(){
+    const navBox = document.querySelector(".menu");
+    let sizeStuf = window.innerWidth;
+    if (sizeStuf < 500 && sizeStuf > 498) {
+        // console.log("sizeStuf");
+        navBox.classList.remove("menu");
+    }
+    // const widthOutput = document.querySelector('body');
+    // const navBox = document.querySelector(".menu");    
+    // widthOutput = window.innerWidth;
+    // if (widthOutput < 600) {
+    //     navBox.classList.remove("menu");
+    // }
+    
+}
+
+
+// element.classList.remove("mystyle");
